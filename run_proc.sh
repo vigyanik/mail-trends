dccde7e8e206adb9ac28e11b3cd37a917f7c288d
Manoj Sharma <vigyanik@gmail.com>
2017-08-30 09:24:43 -0700

    update runner script

---
+++ run_proc.sh
@@ -0,0 +1,7 @@
+python main.py \
+  --server=imap.gmail.com \
+  --use_ssl \
+  --username=vigyanik@gmail.com \
+  --me=vigyanik@gmail.com,manoj.sharma.now@gmail.com \
+  --skip_labels \
+  --max_messages=50000
